e9a04cf2247e4cedb82d8fed94c2b44fa42acd81
Wiley Hays <wdhays2@charter.net>
2015-05-13 19:46:14 -0400

    Heroku config

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,33 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- Your stuff -->
+ <"http://s3.amazonaws.com/codecademy-content/projects/z2d/stories/menu-black.svg">
+    <!-- Include Sidr bundled CSS theme -->
+    <link rel="stylesheet" href="javascripts/sidr/stylesheets/jquery.sidr.dark.css">
+  </head>
+  <body>
+    <!-- Your stuff -->
+< "http://s3.amazonaws.com/codecademy-content/projects/z2d/stories/close-black.svg">
+    <!-- Include jQuery -->
+    <script src="javascripts/jquery.js"></script>
+    <!-- Include the Sidr JS -->
+    <script src="javascripts/sidr/jquery.sidr.min.js"></script>
+  </body>
+</html>
